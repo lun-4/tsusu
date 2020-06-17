130a7836bf15ebbb0784aec61d60f2ecf71ba0d4
Luna <git@l4.pm>
2020-06-17 17:42:47 -0300

    fix typings for stats reader

--- src/process_stats.zig
+++ src/process_stats.zig
@@ -1,27 +1,24 @@
 const std = @import("std");
 
 pub const Stats = struct {
-    cpu_usage: f32
+    cpu_usage: u64
 };
 
 pub const StatsOptions = struct {};
 
 pub const StatsFile = struct {
-    utime: ?[]const u8,
-    stime: ?[]const u8,
-    cutime: ?[]const u8,
-    cstime: ?[]const u8,
-    num_threads: ?[]const u8,
-    starttime: ?[]const u8,
+    utime: u32,
+    stime: u32,
+    cstime: u32,
+    starttime: u32,
 };
 
-// basically, this locks us to libc.
-pub extern "c" fn sysconf(name: c_int) c_longdouble;
-pub const _SC_CLK_TCK = 2;
+// TODO port this from musl
+//pub extern "c" fn sysconf(name: c_int) c_longdouble;
+//pub const _SC_CLK_TCK = 2;
 
 fn readStatsFile(path: []const u8) !StatsFile {
-    var stats = Stats{};
-    var stat_file = try std.fs.cwd().openFile(&path_buffer, .{ .read = true, .write = false });
+    var stat_file = try std.fs.cwd().openFile(path, .{ .read = true, .write = false });
     defer stat_file.close();
 
     // TODO check if [512]u8 is what we want, and also see if we really need
@@ -33,42 +30,39 @@ fn readStatsFile(path: []const u8) !StatsFile {
     var line_it = std.mem.split(stat_line, " ");
 
     // skip pid, comm
-    line_it.skip();
-    line_it.skip();
+    _ = line_it.next();
+    _ = line_it.next();
 
     // maybe we can use state someday?
-    line_it.skip();
+    _ = line_it.next();
 
     // skip ppid, pgrp, session, tty_nr, tgpid, flags, minflt, cminflt,
     // majflt, cmajflt
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
 
     // get utime, stime
     // TODO error handling
-    stats.utime = line_it.next();
-    stats.stime = line_it.next();
-    stats.cutime = line_it.next();
-    stats.cstime = line_it.next();
+    const utime = line_it.next();
+    const stime = line_it.next();
+    const cutime = line_it.next();
+    const cstime = line_it.next();
 
-    // skip priority, nice, num_threads
-    line_it.skip();
-    line_it.skip();
+    // skip priority, nice, num_threads, itrealvalue
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
 
-    stats.num_threads = line_it.skip();
-
-    // skip itrealvalue
-    line_it.skip();
-
-    stats.starttime = line_it.next();
+    const starttime = line_it.next();
 
     const vsize = line_it.next().?;
     const rss = line_it.next().?;
@@ -76,73 +70,80 @@ fn readStatsFile(path: []const u8) !StatsFile {
 
     // skip startcode, endcode, startstack, kstkesp, kstkeip, signal, blocked,
     // sigignore, sigcatch, wchan
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
 
     const nswap = line_it.next().?;
     const cnswap = line_it.next().?;
 
     // skip exit_signal
-    line_it.skip();
+    _ = line_it.next();
 
     const processor = line_it.next().?;
 
     // skip rt_priority, policy, delayacct_blkio_ticks
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
 
     const guest_time = line_it.next().?;
     const cguest_time = line_it.next().?;
 
     // skip start_data, end_data, start_brk, arg_start, arg_end, env_start
     // env_end, exit_code
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
-    line_it.skip();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
+    _ = line_it.next();
 
-    return stats;
+    return StatsFile{
+        .utime = try std.fmt.parseInt(u32, utime.?, 10),
+        .stime = try std.fmt.parseInt(u32, stime.?, 10),
+        .cstime = try std.fmt.parseInt(u32, cstime.?, 10),
+        .starttime = try std.fmt.parseInt(u32, starttime.?, 10),
+    };
 }
 
 // TODO convert to Stats struct as we need to syscall to get _SC_CLK_TCK
 pub fn fetchProcessStats(pid: std.os.pid_t, options: StatsOptions) !Stats {
-    const clock_ticks = sysconf(_SC_CLK_TCK);
+    const clock_ticks = 100;
 
-    var uptime_file = std.fs.cwd().openFile("/proc/uptime", .{ .read = true, .write = false });
+    var uptime_file = try std.fs.cwd().openFile("/proc/uptime", .{ .read = true, .write = false });
     defer uptime_file.close();
 
-    const uptime_str = uptime_file.inStream().readUntilDelimiterOrEof(&uptime_buffer, " ");
-    const uptime_float = try std.fmt.parseFloat(f64, uptime_str);
+    var uptime_buffer: [64]u8 = undefined;
+    const uptime_str = try uptime_file.inStream().readUntilDelimiterOrEof(&uptime_buffer, ' ');
+    const uptime_float = try std.fmt.parseFloat(f64, uptime_str.?);
     const uptime = @floatToInt(u64, uptime_float);
 
     // pids are usually 5 digit, so we can keep a lot of space for them
     var path_buffer: [64]u8 = undefined;
-    const stat_path = std.fmt.bufPrint(&path_buffer, "/proc/{}/stat", .{pid});
+    const stat_path = try std.fmt.bufPrint(&path_buffer, "/proc/{}/stat", .{pid});
 
     // Calculate CPU usage
     const stats1 = try readStatsFile(stat_path);
 
-    const utime = try std.fmt.parseInt(u32, stats1.utime, 10);
-    const stime = try std.fmt.parseInt(u32, stats1.stime, 10);
-    const cstime = try std.fmt.parseInt(u32, stats1.cstime, 10);
-    const starttime = try std.fmt.parseInt(u32, stats1.starttime, 10);
+    const utime = stats1.utime;
+    const stime = stats1.stime;
+    const cstime = stats1.cstime;
+    const starttime = stats1.starttime;
 
     const total_time = utime + stime + cstime;
     const seconds = uptime - (starttime / clock_ticks);
-    const cpu_usage = 100 * ((total_time / clock_ticks) / seconds);
+    const cpu_usage: f64 = @as(f64, 100) *
+        ((@intToFloat(f64, total_time) / @intToFloat(f64, clock_ticks)) / @intToFloat(f64, seconds));
 
-    return Stats{ .cpu_usage = cpu_usage };
+    return Stats{ .cpu_usage = @floatToInt(u64, std.math.floor(cpu_usage)) };
 }
